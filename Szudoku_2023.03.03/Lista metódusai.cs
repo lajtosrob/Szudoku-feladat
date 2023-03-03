8daa6af1654aeb466cfa6daabd51ae95141b4414
lajtosrob <lajtosrobert@freemail.hu>
2023-03-03 12:57:27 +0100

    Add project files.

---
+++ Szudoku_2023.03.03/Lista metódusai.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listak
+{
+    class Program
+    {
+        static void DisplayList<T>(List<T> lista, String message)
+        {
+
+            Console.WriteLine('\n' + new String('─', Console.WindowHeight - 1));
+            Console.WriteLine(message);
+            Console.Write($"Count:{lista.Count} │");
+            foreach (var item in lista)
+                Console.Write(item.ToString() + '│');
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+
+        {
+            //LISTA ADATSZERKEZET
+            //
+            //Használatához szükséges a 
+            //using System.Collections.Generic;
+
+            //Megjegyzés: A System.Collections számos további adatszerkezetet is tartalmaz
+            // - System.Collections.Stack - Verem
+            // - System.Collections.Queue - Sor
+            // - System.Collections.SortedList - Rendezett lista
+            // - System.Collections.ArrayList - Dinamikus tömb
+
+            //1. DEKLARÁCIÓ és INICIALIZÁLÁS ────────────────────────────────────────────────────────────────
+            // [TÖMB] deklarálása és inicializálása
+            int[] szamokTomb;
+            szamokTomb = new int[100]; // tömb inicializálása 100 elemre
+
+
+            //Lista deklarálása
+            List<int> szamokLista;
+            szamokLista = new List<int>(); //inicializálás, eredménye egy üres lista
+
+            //Az előző üres lista megszűnik, helyette új jön létre a megadott értékekkel
+            szamokLista = new List<int> { 12, 11, 345, 25, -47, 456 };
+            DisplayList(szamokLista, "1. Alap:");
+
+            //Inicializálás kezdeti elemszám megadásával. A Count értéke továbbra is 0 marad!
+            //A tárkezelés optimalizálása miatt hasznos. Kevésbé erőforrásigényes 20 elemnek egyszerre
+            //memóriahelyet foglalni, mint 20 alkalommal egynek.
+            List<double> masikLista = new List<double>(20);
+
+            //inicializálás az értékek megadásával
+            List<String> gyumolcsok = new List<string>() { "alma", "körte", "szilva", "barack", "füge" };
+            DisplayList(gyumolcsok, "1. Alap:");
+
+            //2. ELEMSZÁM ───────────────────────────────────────────────────────────────────────────────────
+
+            // [TÖMB]
+            // Console.WriteLine(szamokTomb.Length);
+
+            // [LISTA, SZÓTÁR]
+            Console.WriteLine(szamokLista.Count); //elemek száma
+
+            //3. ELEM ELÉRÉSE és MÓDOSÍTÁSA ─────────────────────────────────────────────────────────────────
+            // [TÖMB, LISTA]
+            //szamokTomb[12] = 122;  //tömb elemének módosítása
+            gyumolcsok[1] = "banán"; //A "körte" helyére "banán" kerül
+
+            //4. ÚJ ELEM FELVÉTELE ──────────────────────────────────────────────────────────────────────────
+            // [TÖMB] NEM lehetséges!
+
+            // [LISTA]
+            // Felvétel a lista végére
+            szamokLista.Add(122);
+            gyumolcsok.Add("szőlő");
+            DisplayList(gyumolcsok, "4. Add után:");
+
+            // Beszúrás egy elem elé
+            gyumolcsok.Insert(2, "narancs"); //0-tól indexelés mellett a megadott indexű elem elé szúr be
+            DisplayList(gyumolcsok, "Insert után [2-es indexű elé]:");
+
+            //5. ELEM TÖRLÉSE ───────────────────────────────────────────────────────────────────────────────
+            // [TÖMB] NEM lehetséges!
+
+            // [LISTA]
+            //Érték eltávolítása listából
+            // a) index segítségével távolítja el az elemet
+            gyumolcsok.RemoveAt(3);
+
+            // b) a törlendő elemet adjuk meg
+            // bool érték határozza meg a sikerességet /true=sikeres/
+            if (gyumolcsok.Remove("füge") == true)
+            {
+                Console.WriteLine("Sikeres a törlés, mivel volt ilyen elem!");
+            }
+            else
+            {
+                Console.WriteLine("A törlés sikertelen!");
+            }
+
+            DisplayList(gyumolcsok, "5. A 3-as indexű és a \"füge\" törlése után:");
+
+            Console.WriteLine("Tovább...");
+            Console.ReadKey();
+
+            //6. KERESÉS az ADATSZERKEZETBEN ────────────────────────────────────────────────────────────────
+            // [TÖMB] NEM lehetséges!
+
+            // [LISTA]
+            //A keresett elem helyét adja vissza. Ha nem létezik akkor -1-et
+            Console.WriteLine($"6. Milyen indexű a \"narancs\" elem (ha van) :{gyumolcsok.IndexOf("narancs")}");
+
+            // gyumolcsok.LastIndexOf("körte")  - a keresést a lista végéről kezdi
+            //gyumolcsok.IndexOf("alma", 3); - az "alma" elemet a 3-as indexű elemtől kezdi keresni (elejéről a vége felé halad)
+
+            //Eldönti, hogy a megadott elem benne van-e a listában?
+            if (gyumolcsok.Contains("mandarin"))
+            {
+                Console.WriteLine("6. Van a gyümölcsök között \"mandarin\"!");
+            }
+            else
+            {
+                Console.WriteLine("6. Nincs a gyümölcsök között \"mandarin\"!");
+            }
+
+            //7. ADATSZERKEZET RENDEZÉSE ────────────────────────────────────────────────────────────────────
+            // [TÖMB] NEM lehetséges!
+
+            // [LISTA]
+            szamokLista.Sort(); //Növekvő sorrendbe rendezi
+            DisplayList(szamokLista, "7. Rendezés után a számok:");
+            gyumolcsok.Sort();
+            DisplayList(gyumolcsok, "7. Rendezés után a gyümölcsök:");
+
+            //8. ADATSZERKEZET TÖRLÉSE ──────────────────────────────────────────────────────────────────────
+            // [TÖMB] NEM lehetséges!
+            gyumolcsok.Clear();
+            DisplayList(gyumolcsok, "8. Törlés után:");
+
+            //9. EGYÉB ÉRDEKES ──────────────────────────────────────────────────────────────────────────────
+            szamokLista.Insert(3, -264);
+            szamokLista.Insert(6, 10);
+            DisplayList(szamokLista, "9. Két elemmel bővítve: -264; 10;");
+            szamokLista.Reverse(); //Elemek sorrendjének megfordítása. Az elsőből utolsó lesz és fordítva.
+            DisplayList(szamokLista, "9. Megfordítás:");
+
+        }
+    }
+}
---
+++ Szudoku_2023.03.03/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+
+StreamReader sr = new StreamReader("C:\\Users\\LifebookE736\\source\\repos\\Szudoku_2023.03.03\\Szudoku_2023.03.03\\DATA\\feladvanyok.txt");
+List<Feladvany> sorok = new List<Feladvany>();  
+
+while (!sr.EndOfStream)
+{
+    Feladvany line = new Feladvany(sr.ReadLine());
+    sorok.Add(line);
+}
+
+sr.Close();
+
+Console.WriteLine($"3. feladat: Beolvasva {sorok.Count} feladvány");
+
+int feladvanySzamlalo = 0;
+int feladvanyMeret = 0;
+
+do
+{
+    Console.Write("\n4. feladat: Kérem a feladvány méretét [4..9]: ");
+    feladvanyMeret = Convert.ToInt32(Console.ReadLine());
+
+} while (feladvanyMeret > 9 || feladvanyMeret <  4);
+
+foreach (var line in sorok)
+{
+    if (feladvanyMeret == line.Meret)
+    {
+        feladvanySzamlalo++;
+    }
+}
+
+Console.WriteLine($"{feladvanyMeret}x{feladvanyMeret} méretű feladványból {feladvanySzamlalo} darab van tárolva");
+
+Random rnd= new Random();
+int randomSzam = rnd.Next(feladvanySzamlalo);
+
+int randomSzamlalo = 0;
+Feladvany kivalasztottSor;
+int kitoltottseg = 0;
+
+
+foreach (var sor in sorok)
+{
+    if (sor.Meret == feladvanyMeret)
+    {
+        randomSzamlalo++;
+        if (randomSzamlalo == randomSzam)
+        {
+            kivalasztottSor = sor;
+            Console.WriteLine($"\n5. feladat: A kiválasztott feladvány: \n{kivalasztottSor.Kezdo}");
+            foreach (var item in kivalasztottSor.Kezdo)
+            {
+                if(item != '0')
+                {
+                    kitoltottseg++;
+                }
+            }
+            Console.WriteLine($"\n6. feladat: A feladvány kitöltöttsége: {(Convert.ToDouble(kitoltottseg))/(Convert.ToDouble(kivalasztottSor.Kezdo.Length))*100}%");
+
+            Console.WriteLine("\n7. feladat: A feladvány kirajzolva");
+
+            kivalasztottSor.Kirajzol();
+
+            continue;
+        }
+    }
+}
+
+List<Feladvany> kivalasztottList = new List<Feladvany>();
+
+StreamWriter sw = new StreamWriter("sudoku" + feladvanyMeret + ".txt");
+
+foreach (var sor in sorok)
+{
+    if(feladvanyMeret == sor.Meret)
+    {
+        sw.WriteLine(sor.Kezdo);
+    }
+}
+sw.Close();
+string allomanyNev = "sudoku" + feladvanyMeret + ".txt";
+Console.WriteLine($"\n8. feladat: {allomanyNev} állomány {feladvanySzamlalo} darab feladvánnyal létrehozva ");
+
+
+
+
+
